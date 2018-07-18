b92a098e73503bfd183996f96439a9f9b2fa5aec
ecssiah <ecssiah@gmail.com>
2018-07-18 03:42:54 -0500

    Clean up AI classes

--- lib/ai/cd_ai.rb
+++ lib/ai/cd_ai.rb
@@ -102,6 +102,7 @@ class AI
     @token = token
   end
 
+
   def calculate_move
     @info = MoveInfo.new(@game, @token)
 
@@ -110,6 +111,7 @@ class AI
     move += 1
     move.to_s
   end
+
 
   def decision_order
     if (m = decide_first_move)
@@ -123,39 +125,43 @@ class AI
     end
   end
 
+
+  # Chooses the cells that are threats if available.
+  def prioritize_threats(combo, possible_moves)
+    if @info.threats
+      possible_moves << @info.threats.select { |i| combo.include?(i) }
+    else
+      possible_moves << combo.select { |i| @game.board.cells[i]==" "}
+    end
+  end 
+
+
   def find_best_move
     best_move = []
     possible_moves = []
 
     Game::WIN_COMBINATIONS.each do |combo|
-      row_contents = combo.collect {|i| @game.board.cells[i] }
-      #Chooses the row with itself and 2 spaces.
-      if row_contents.include?(@info.token) && row_contents.select.count{|i| i==" "} >= 2
-        #Chooses the cells that are threats if available.
-        if @info.threats
-          possible_moves << @info.threats.select {|i| combo.include?(i) }
-        else
-          possible_moves << combo.select {|i| @game.board.cells[i]==" "}
-        end
-      #Then chooses the row with itself and empty space if others have failed.
-      elsif row_contents.include?(@info.token) && row_contents.include?(" ")
-        #Again priority to cells that are threats.
-        if @info.threats
-          possible_moves << @info.threats.select {|i| combo.include?(i) }
-        else
-          possible_moves << combo.select {|i| @game.board.cells[i]==" "}
-        end
+      row_contents = combo.collect { |i| @game.board.cells[i] }
+
+      # Choose combo with player token and empty spaces.
+      combo_has_token = row_contents.include?(@info.token)
+      combo_has_empty = row_contents.any? { |i| i == ' ' }
+
+      if combo_has_token && combo_has_empty
+        prioritize_threats(combo, possible_moves)
       end
     end
-    #Chooses corners if available.
-    if possible_moves.flatten.any?{|i| [0,2,6,8].include?(i)}
-      best_move = possible_moves.flatten.select {|i| [0,2,6,8].include?(i)}
+
+    # Chooses corners if available.
+    if possible_moves.flatten.any? { |i| [0,2,6,8].include?(i) }
+      best_move = possible_moves.flatten.select { |i| [0,2,6,8].include?(i) }
     else
       best_move = possible_moves
     end
-    #If all else has failed. Chooses a empty space.
+
+    # If all else has failed. Chooses a empty space.
     if possible_moves.empty?
-      best_move = @game.board.cells.select{|i| i == " "}
+      best_move = @game.board.cells.select { |i| i == " " }
     end
 
     best_move.flatten!
@@ -163,17 +169,19 @@ class AI
     best_move.count > 1 ? best_move[rand(0...best_move.count - 1)] : best_move[0]
   end
 
+
   def decide_first_move
     if first_move_on_board?
       moves = ["0", "0", "0", "2", "4", "4"]
       return moves[rand(0...moves.count - 1)]
     elsif @info.enemy_locations.count == 1 && @info.my_locations.count == 0
-      return "4" if [0,2,6,8].any?{ |i| @info.enemy_locations.include?(i) }
+      return "4" if [0,2,6,8].any? { |i| @info.enemy_locations.include?(i) }
     end
   end
 
+
   def first_move_on_board?
-    @game.board.cells.all? {|cell| cell == " "}
+    @game.board.cells.all? { |cell| cell == " " }
   end
 end#endof class
 
